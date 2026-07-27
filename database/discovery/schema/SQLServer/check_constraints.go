71bdd37a56db60d49e28bb3819334c16422f26dd
Hennie5229x <henrico.battlefield@gmail.com>
2026-07-27 18:48:28 +0200

    query formatting

--- database/discovery/schema/SQLServer/check_constraints.go
+++ database/discovery/schema/SQLServer/check_constraints.go
@@ -8,12 +8,11 @@ func SqlServerCheckConstraints(db *gorm.DB, tableName string) []CheckConstraint 
 	var checkConstraints []CheckConstraint
 
 	err := db.Raw(`
-		SELECT
-			cc.object_id AS ConstraintObjectID,
-			cc.definition AS Definition
-		FROM sys.check_constraints cc
-		WHERE cc.parent_object_id = OBJECT_ID(?, 'U')
-		ORDER BY cc.object_id;
+		SELECT		cc.object_id AS ConstraintObjectID,
+					cc.definition AS Definition
+		FROM 		sys.check_constraints cc
+		WHERE 		cc.parent_object_id = OBJECT_ID(?, 'U')
+		ORDER BY 	cc.object_id;
 	`, tableName).Scan(&checkConstraints).Error
 
 	if err != nil {
--- database/discovery/schema/SQLServer/primary_keys.go
+++ database/discovery/schema/SQLServer/primary_keys.go
@@ -8,25 +8,21 @@ func SqlServerPrimaryKeys(db *gorm.DB, tableName string) []PrimaryKeyColumn {
 	var primaryKeys []PrimaryKeyColumn
 
 	err := db.Raw(`
-		SELECT
-			kc.object_id AS ConstraintObjectID,
-			CASE WHEN i.type = 1 THEN 'CLUSTERED' ELSE 'NONCLUSTERED' END AS IndexType,
-			c.name AS ColumnName,
-			ic.key_ordinal AS KeyOrdinal,
-			ic.is_descending_key AS IsDescending
-		FROM sys.key_constraints kc
-		JOIN sys.indexes i
-			ON i.object_id = kc.parent_object_id
-			AND i.index_id = kc.unique_index_id
-		JOIN sys.index_columns ic
-			ON ic.object_id = i.object_id
-			AND ic.index_id = i.index_id
-		JOIN sys.columns c
-			ON c.object_id = ic.object_id
-			AND c.column_id = ic.column_id
-		WHERE kc.parent_object_id = OBJECT_ID(?, 'U')
-			AND kc.type = 'PK'
-		ORDER BY ic.key_ordinal;
+		SELECT		kc.object_id AS ConstraintObjectID,
+					CASE WHEN i.type = 1 THEN 'CLUSTERED' ELSE 'NONCLUSTERED' END AS IndexType,
+					c.name AS ColumnName,
+					ic.key_ordinal AS KeyOrdinal,
+					ic.is_descending_key AS IsDescending
+		FROM 		sys.key_constraints kc
+		JOIN 		sys.indexes i ON i.object_id = kc.parent_object_id
+					AND i.index_id = kc.unique_index_id
+		JOIN 		sys.index_columns ic ON ic.object_id = i.object_id
+					AND ic.index_id = i.index_id
+		JOIN 		sys.columns c ON c.object_id = ic.object_id
+					AND c.column_id = ic.column_id
+		WHERE 		kc.parent_object_id = OBJECT_ID(?, 'U')
+		AND 		kc.type = 'PK'
+		ORDER BY 	ic.key_ordinal;
 	`, tableName).Scan(&primaryKeys).Error
 
 	if err != nil {
