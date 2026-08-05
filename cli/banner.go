96276b7b51c6c2d44a41adc1ab8aa628d4172d84
Hennie5229x <henrico.battlefield@gmail.com>
2026-08-05 21:13:43 +0200

    banner more pretty now

--- cli/banner.go
+++ cli/banner.go
@@ -3,35 +3,63 @@ package cli
 import (
 	"blueprint/appinfo"
 	"fmt"
+	"regexp"
 	"strings"
+	"unicode/utf8"
 )
 
-func center(text string, width int) string {
-	padding := width - len(text)
-	left := padding / 2
-	right := padding - left
+const (
+	Reset = "\033[0m"
+	Cyan  = "\033[96m"
+)
 
-	return strings.Repeat(" ", left) +
+var ansiRegex = regexp.MustCompile(`\x1b\[[0-9;]*m`)
+
+func cyan(text string) string {
+	return Cyan + text + Reset
+}
+
+func textWidth(text string) int {
+	plain := ansiRegex.ReplaceAllString(text, "")
+	return utf8.RuneCountInString(plain)
+}
+
+func center(text string, width int) string {
+	padding := width - textWidth(text)
+
+	if padding < 0 {
+		padding = 0
+	}
+
+	leftPadding := padding / 2
+	rightPadding := padding - leftPadding
+
+	return strings.Repeat(" ", leftPadding) +
 		text +
-		strings.Repeat(" ", right)
+		strings.Repeat(" ", rightPadding)
 }
 
 func left(text string, width int) string {
-	padding := width - len(text)
-	left := 1
-	right := padding - left
+	padding := width - textWidth(text)
 
-	return strings.Repeat(" ", left) +
+	if padding < 1 {
+		padding = 1
+	}
+
+	leftPadding := 1
+	rightPadding := padding - leftPadding
+
+	return strings.Repeat(" ", leftPadding) +
 		text +
-		strings.Repeat(" ", right)
+		strings.Repeat(" ", rightPadding)
 }
 
 func PrintBanner() {
 	fmt.Println("╔══════════════════════════════════════╗")
-	fmt.Printf("║%s║\n", center(appinfo.Name, appinfo.BannerWidth))
+	fmt.Printf("║%s║\n", center(cyan("◆ "+appinfo.Name+" ◆"), appinfo.BannerWidth))
 	fmt.Printf("║%s║\n", center(appinfo.Version, appinfo.BannerWidth))
 	fmt.Printf("║%s║\n", center("", appinfo.BannerWidth))
-	fmt.Printf("║%s║\n", left("Commands list = 'blue help'", appinfo.BannerWidth))
+	fmt.Printf("║%s║\n", left("➜ blue help", appinfo.BannerWidth))
 	fmt.Println("╚══════════════════════════════════════╝")
 	fmt.Println()
 }
