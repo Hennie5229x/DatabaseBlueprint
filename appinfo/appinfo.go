65ca256c3c2de74f35f4d82416f7a31c573a54dd
Hennie5229x <henrico.battlefield@gmail.com>
2026-08-05 15:27:53 +0200

    version numbering for build

--- appinfo/appinfo.go
+++ appinfo/appinfo.go
@@ -2,8 +2,9 @@ package appinfo
 
 const (
 	Name            = "Database Blueprint"
-	Version         = "v0.0.1"
 	BannerWidth     = 38
 	ConnectionsFile = "config.json"
 	ScriptDirectory = "Output"
 )
+
+var Version = "dev"
